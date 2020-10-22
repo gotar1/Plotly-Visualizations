7275b6e7495199b1f775f99472b7baf7fa0f26da
Gotar <eltahir.g@gmail.com>
2020-10-22 18:36:19 -0500

    working code

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,36 @@
 // working in this homework was a pain in...
 
-// function barPlotUpdate(id) {
+function createPlots(id) {
 
     d3.json("../data/samples.json").then((data) =>{
         let dataCopy = data;
         let dataSample = dataCopy.samples;
         console.log(dataSample);
 
-        let mapedData = dataSample.map(object => object)[0];
-        console.log(mapedData);
+        let filteredData = dataSample.filter(object => object.id === id);
+        console.log(filteredData);
    
-        let otuIds = mapedData.otu_ids;
+        let otuIds = filteredData.otu_ids;
         console.log(otuIds);
         let otuSliced =  otuIds.slice(0, 10);
         console.log(otuSliced);
-        let funk = `"OTU" ${otuSliced}`
-        console.log(funk)
-        let otuLabels = mapedData.otu_labels;
+        let otuIdNamed = otuSliced.map(id => "OTU " + id);
+        console.log(otuIdNamed)
+
+        let otuLabels = filteredData.otu_labels;
         console.log(otuLabels);
 
-        let sampleValues = mapedData.sample_values;
+        let sampleValues = filteredData.sample_values;
         console.log(sampleValues);
      
         let trace1 = {
             x: sampleValues.slice(0, 10).reverse(),
-            y: `"OTU" ${otuSliced}`,
+            y: otuIdNamed.reverse(),
             orientation: 'h',
             type: 'bar',
             text: otuLabels.slice(0, 10).reverse()
         };
-        // console.log(otuLabels.slice(0, 10))
-        // console.log(text)
+
         let dataTrace = [trace1];
     
         let layout = {
@@ -44,8 +44,82 @@
         };
     
        Plotly.newPlot('bar', dataTrace, layout); 
+
+       let bubbleTrace = {
+           x: otuIds,
+           y: sampleValues,
+           mode: "markers",
+           marker: {
+               size: sampleValues,
+               color: otuIds
+           },
+           text: otuLabels
+       };
+
+       let bubbleData = [bubbleTrace];
+
+       let bubbleLayout = {
+           xaxis: {title: "OTU ID"},
+           height: 500,
+           width: 900,
+           title: "Bacteria Concentration"
+
+       };
+
+       Plotly.newPlot('bubble', bubbleData, bubbleLayout)
     })
-// }
+}
+
+
+function createDemographic(id) {
+    d3.json("../data/samples.json").then(data => {
+         
+        let metaData = data.metadata;
+        console.log(metaData)
+
+        let filteredMetaData = metaData.filter(data => data.id === id);
+        console.log(filteredMetaData)
+
+        let demographicInfo = d3.select("#sample-metadata");
+        console.log(demographicInfo)
+        demographicInfo.html("");
+        Object.entries(filteredMetaData).forEach(key => {
+            demographicInfo
+                .append("h5")
+                .text(key)
+            console.log(key)
+        });
+
+    });
+
+
+};
+
+function optionChanged(id) {
+    createPlots(id);
+    createDemographic(id);
+}
+
+function init() {
+    let testIdButton = d3.select("#selDataset");
+    d3.json("../data/samples.json").then(data => {
+        // let dataCopy1 = data;
+        // let dataCopy2 = JSON.parse(dataCopy1);
+        // data = JSON.parse(data)
+        data.names.forEach(name => {
+            testIdButton
+                .append("option")
+                .property("value")
+                .text(name)
+        });
+        
+        createPlots(data.names[0]);
+        createDemographic(data.names[0])
+    })
+
+}
+
+init()
 
 // let x = d3.json()
 // console.log(x)
