459984b4e5f74b6907caeb586b75954ce133b551
Gotar <eltahir.g@gmail.com>
2020-10-22 00:21:06 -0500

    stuff

--- static/js/app.js
+++ static/js/app.js
@@ -2,46 +2,48 @@
 
 // function barPlotUpdate(id) {
 
-    d3.json("../data/samples.json").then(data => {
+    d3.json("../data/samples.json").then((data) =>{
         let dataCopy = data;
         let dataSample = dataCopy.samples;
         console.log(dataSample);
-        let otuIds = dataSample.map(object => object.otu_ids);
+
+        let mapedData = dataSample.map(object => object)[0];
+        console.log(mapedData);
+   
+        let otuIds = mapedData.otu_ids;
         console.log(otuIds);
-        // let trace = otuIds.data
-        // console.log(trace)
-        // let sampleValues = dataSample.map(Object => Object.sample_values);
-        // console.log(sampleValues);
-        // let otuLabels = dataSample.map(Object => Object.otu_labels);
-        // let otuIds = dataSample.filter(data => data.otu_ids);
-        // console.log(otuIds);
-        // console.log(otuLabels);
-        // console.log(otuLabels.property('value')
-        // let filteredData = dataSample.filter(data => data.id.toString()===id);
-        // console.log(trace1)
-        // let slicedData = otuIds.slice(0, 10);
-        // console.log(slicedData);
-    //     let trace1 = {
-    //         x: slicedData.map(Object => Object.sample_values),
-    //         y: slicedData.map(Object => Object.otu_ids),
-    //         orientation: 'h',
-    //         tybe: 'bar',
-    //         text: slicedData.map(Object => Object.otu_labels)
-    //     };
+        let otuSliced =  otuIds.slice(0, 10);
+        console.log(otuSliced);
+        let funk = `"OTU" ${otuSliced}`
+        console.log(funk)
+        let otuLabels = mapedData.otu_labels;
+        console.log(otuLabels);
+
+        let sampleValues = mapedData.sample_values;
+        console.log(sampleValues);
+     
+        let trace1 = {
+            x: sampleValues.slice(0, 10).reverse(),
+            y: `"OTU" ${otuSliced}`,
+            orientation: 'h',
+            type: 'bar',
+            text: otuLabels.slice(0, 10).reverse()
+        };
+        // console.log(otuLabels.slice(0, 10))
+        // console.log(text)
+        let dataTrace = [trace1];
     
-    //     let dataTrace = [trace1];
+        let layout = {
+            title: "bully button mess",
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            }
+        };
     
-    //     let layout = {
-    //         title: "bully button mess",
-    //         margin: {
-    //             l: 100,
-    //             r: 100,
-    //             t: 100,
-    //             b: 100
-    //         }
-    //     }
-    
-    //    Plotly.newPlot('bar', dataTrace, layout); 
+       Plotly.newPlot('bar', dataTrace, layout); 
     })
 // }
 
